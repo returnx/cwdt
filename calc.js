9e18583ad174bc3001bd3506a46eeb218be026e5
Kulinda <39636810+kulinda@users.noreply.github.com>
2023-12-23 15:41:42 +0100

    simplify form handling
    Putting the data extraction and number conversion into a helper function makes the actual formulas easier to read and maintain, and it reduces total code size.
    
    Some names of form fields or variables were adjusted to match.

--- calc.js
+++ calc.js
@@ -2,11 +2,11 @@
 // It doesn't need to wait for images etc (as body.onLoad would do)
 function init() {
      // All input elements shall trigger recalculation when changed
-     for (let e of document.getElementsByTagName("input")) {
-          e.addEventListener("change", checkLoop);
-          e.addEventListener("input", checkLoop);
+     for (const e of document.getElementsByTagName("input")) {
+          e.addEventListener("change", checkEverything);
+          e.addEventListener("input", checkEverything);
      }
-     checkLoop();
+     checkEverything();
 }
 if (document.readyState === "loading") {
      document.addEventListener("DOMContentLoaded", init);
@@ -14,31 +14,46 @@ if (document.readyState === "loading") {
      init();
 }
 
-function checkLoop() {
-     const life = document.getElementById("life").value;
-     const energyShield = document.getElementById("energyShield").value;
-     const chaosRes = document.getElementById("chaos").value;
-
-     const ringCount1 = document.getElementById("ringCount1").checked;
-
-     let ringMultiplier;
-     if (ringCount1 == true) {
-          ringMultiplier = 1;
-     } else {
-          ringMultiplier = 2;
+// Collects all values entered into a <form>
+function readFormElements(formid) {
+     const values = {};
+     const form = document.getElementById(formid);
+     for (const input of form.elements) {
+          if (!(input instanceof HTMLInputElement)) continue;
+          if (input.type === "radio" && !input.checked) continue;
+          const name = input.name;
+          if (!name) continue;
+          const value = Number(input.value);
+          values[name] = value;
      }
+     return values;
+}
 
-     // const isDivergent = document.getElementById("typeDivergent").checked
-     const gemQuality = document.getElementById("CWDTQuality").value;
-     const gemLevel = document.getElementById("CWDTLevel").value;
-     const ringDamage = document.getElementById("ringDamage").value;
-     const SummonSkeletonLevel = document.getElementById("SummonSkeletonLevel").value;
+function checkEverything() {
+     checkLoop();
+     checkFlask();
+}
+
+// Calculates whether the loop works
+function checkLoop() {
+     const {
+          life,
+          energyShield,
+          chaosRes,
+          ringCount,
+          ringDamage,
+          SummonSkeletonLevel,
+          CWDTLevel,
+          CWDTQuality,
+          ward
+     } = readFormElements("loopForm");
+
      let skeletonCount = 2;
      if (SummonSkeletonLevel >= 11) skeletonCount++;
      if (SummonSkeletonLevel >= 21) skeletonCount++;
      if (SummonSkeletonLevel >= 31) skeletonCount++;
 
-     let skeletonDamage = ringMultiplier * ringDamage * skeletonCount;
+     let skeletonDamage = ringCount * ringDamage * skeletonCount;
      document.getElementById("skelDamage").innerHTML = skeletonDamage;
 
      let frDamage = (life * 0.4 + energyShield * 0.25) * (1 - (chaosRes / 100));
@@ -49,7 +64,7 @@ function checkLoop() {
 
      let threshold;
      let status = false;
-     let gLevel = parseInt(gemLevel);
+     let gLevel = parseInt(CWDTLevel);
 
      // if(parseInt(SummonSkeletonLevel) > gLevel) {
      //     gLevel = parseInt(SummonSkeletonLevel);
@@ -83,7 +98,7 @@ function checkLoop() {
           default: threshold = 3580; // level 21
      }
 
-     let gemMulti = Math.floor(gemQuality / 2);
+     let gemMulti = Math.floor(CWDTQuality / 2);
      threshold = threshold * (1 - gemMulti / 100);
 
 
@@ -100,8 +115,6 @@ function checkLoop() {
           document.getElementById("status").style.fontWeight = "900";
      }
 
-     const ward = document.getElementById("Ward").value;
-
      if (ward >= frDamage) {
           document.getElementById("wardfr").innerHTML = "YES";
           document.getElementById("wardfr").style.color = "lime";
@@ -111,45 +124,26 @@ function checkLoop() {
           document.getElementById("wardfr").style.color = "yellow";
           document.getElementById("wardfr").style.fontWeight = "900";
      }
+}
 
-     // Flask math
-
-     const asc = document.getElementById("others").checked;
-     var chargesGained = document.getElementById("charges").value;
-     var duration = document.getElementById("duration").value;
-     var reduced = document.getElementById("reduced").value;
-     var olduration = document.getElementById("olduration").value;
-     var olused = document.getElementById("olused").value;
-     var charms = document.getElementById("charms").value
-
-     chargesGained = parseInt(chargesGained);
-     duration = parseInt(duration);
-     reduced = parseInt(reduced);
-     olduration = parseFloat(olduration);
-     olused = parseInt(olused);
-
-     var flaskCount = 0;
-     var flask2 = document.getElementById("2flask").checked;
-     var flask3 = document.getElementById("3flask").checked;
-     var flask4 = document.getElementById("4flask").checked;
-
-     if (flask2 == true) flaskCount = 2;
-     if (flask3 == true) flaskCount = 3;
-     if (flask4 == true) flaskCount = 4;
+// Calculates flask uptime
+function checkFlask() {
+     const {
+          ascCharges,
+          flaskCount,
+          charms,
+          charges,
+          duration,
+          reduced,
+          olduration,
+          olused,
+     } = readFormElements("flaskForm");
 
      var flaskMultiplier = 5 - flaskCount;
 
      // (((8/5)+(1/3)+(3/3))*(1+(R1/100))+0.075) / (R5*(1-R3/100)/(R4*(1+(R2/100))))
 
-     var ascCharges;
-     if (asc == true) {
-          ascCharges = 0;
-     } else {
-          // pathfinder
-          ascCharges = 1;
-     }
-
-     var result = (((4 * flaskMultiplier / 5) + ascCharges + charms / 3) * (1 + (chargesGained / 100)) + 0.075) / (olused * (1 - reduced / 100) / (olduration * (1 + (duration / 100))));
+     var result = (((4 * flaskMultiplier / 5) + ascCharges + charms / 3) * (1 + (charges / 100)) + 0.075) / (olused * (1 - reduced / 100) / (olduration * (1 + (duration / 100))));
      if (result > 1.02) {
           document.getElementById("fstatus").innerHTML = "FLASKS WORK!";
           document.getElementById("fstatus").style.color = "lime";
